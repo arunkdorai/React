ad95059265535655960b54ea9626b23d7485f121
arunkdorai <arunkumar.k0786@gmail.com>
2025-01-09 16:13:12 +0530

    Nested Div with h1 and h2 tags inside child div

--- 01-Inception/App.js
+++ 01-Inception/App.js
@@ -3,6 +3,7 @@
 <div id = "parent">
     <div id = "child">
         <h1> I'm h1 tag</h1>
+        <h2> I'm h2 tag</h2>
     </div>
 </div>
 
@@ -13,11 +14,10 @@ ReactElement(Object) => HTML(Brower Understands)
 const parent = React.createElement(
   "div",
   { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "I'm an h1 tag")
-  )
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+  ])
 );
 
 const heading = React.createElement(
