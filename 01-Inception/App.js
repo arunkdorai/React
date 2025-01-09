02c56d69b55cd283e0437df9ad82d3e1826ded6b
arunkdorai <arunkumar.k0786@gmail.com>
2025-01-09 16:11:16 +0530

    Nested div's with h1 tag

--- 01-Inception/App.js
+++ 01-Inception/App.js
@@ -1,5 +1,31 @@
-const heading = React.createElement("h1", { id: "heading" }, "Hello World from using React CDN");
+/*
+
+<div id = "parent">
+    <div id = "child">
+        <h1> I'm h1 tag</h1>
+    </div>
+</div>
+
+ReactElement(Object) => HTML(Brower Understands)
+
+*/
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I'm an h1 tag")
+  )
+);
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from using React CDN"
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
