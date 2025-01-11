ca296778c5246b5064649a9b8691edd5b5c361f9
arunkdorai <arunkumar.k0786@gmail.com>
2025-01-11 13:09:16 +0530

    Commit before React component

--- Namaste-React/App.js
+++ Namaste-React/App.js
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 
 // JSX - HTML or XML like syntax - (transplied before it reaches the JS) - PARCEL - Babel
 
-// JSX => React.createElement => ReactElement - JS Object => HTMLElement(render)
+// JSX => Babel transpiles it to React.createElement => ReactElement - JS Object => HTMLElement(render)
 
-const jsxHeading = <h1 id="heading">Namaste react using JSX</h1>;
+const jsxHeading = (
+  <h1 className="head" tabIndex={5}>
+    Namaste react using JSX
+  </h1>
+);
 
 console.log(jsxHeading);
 
