51c1d35c7217d0b043f3813f52b3bcf99f752d52
arunkdorai <arunkumar.k0786@gmail.com>
2025-01-11 15:47:44 +0530

    Component Composition

--- Namaste-React/App.js
+++ Namaste-React/App.js
@@ -1,34 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Element
-
-const heading = (
-  <h1 className="head" tabIndex={5}>
+const Title = () => (
+  <h1 className="head" tabIndex="5">
     Namaste react using JSX
   </h1>
 );
 
-/*
-* React Component
+// * Component Composition
 
-! 1. Class Based Component  - Old way to write React components
-? 2. Functional Component - New way to write React components
-
-*/
-
-// * Functional Component
-
-const HeadingComponent = () => {
-  return <h1>Namaste React Functional Component</h1>;
-};
-
-const HeadingComponent2 = () => (
-  <div>
-    <h1 className="heading">Functional Component 2</h1>
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1 className="heading">Namaste React Functional Component</h1>
   </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent2 />);
+root.render(<HeadingComponent />);
